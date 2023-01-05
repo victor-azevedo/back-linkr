890cb3789aaf9b931d48cd0c3ac85e579ff55b3b
Victor Azevedo <josevcazevedo@gmail.com>
2023-01-05 20:16:07 -0300

    feat: add res.local.user at authentication

--- src/middlewares/existsUserValidation.middleware.js
+++ src/middlewares/existsUserValidation.middleware.js
@@ -1,20 +1,21 @@
 import connection from "../database/db.js";
 
-export async function existsUserValidation(req, res, next){
-    const newUser = req.body;
-    
-    try{
-        const user = await connection.query(
-            "SELECT email FROM users WHERE email=$1",
-            [newUser.email]
-        );
+export async function existsUserValidation(req, res, next) {
+  const newUser = req.body;
 
-        if(user.rows[0]){
-            return res.status(409).send({ message: "Esse email já existe" })
-        } 
-    }catch (error) {
-        return res.sendStatus(500);
-      }
+  try {
+    const user = await connection.query(
+      `SELECT "id", "username", "email", "pictureUrl"  FROM users WHERE email=$1`,
+      [newUser.email]
+    );
 
-      next();
-};
+    if (user.rows[0]) {
+      return res.status(409).send({ message: "Esse email já existe" });
+    }
+    res.locals.user = user;
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+
+  next();
+}
