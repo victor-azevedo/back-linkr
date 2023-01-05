78e3a1aab013f64058671a944e24a9bf051d28f7
Luhenrique09 <luhenrique1100@gmail.com>
2023-01-04 23:12:42 -0300

    feat: signup model and midlleware

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routesAuth);
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { signUp } from "../controllers/auth.controllers.js";
+import { signUpSchemaValidation } from "../middlewares/signUpSchemaValidation.middleware.js";
 
 const router = Router();
 
-router.post("/signup", signUp);
+router.post("/signup",signUpSchemaValidation, signUp);
 
 export default router;
