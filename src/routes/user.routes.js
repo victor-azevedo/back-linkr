84d5538bc09eb1b93fe0c609906c74242e376f45
Yuri Desideri <yuridesideri12@gmail.com>
2023-01-06 13:22:18 -0300

    feat: added authValidation middleware to get /user/:id route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getUserInUserPage, searchUserQuery } from "../controllers/user.controllers.js";
+import { authValidation } from "../middlewares/authValidation.middleware.js";
 import { validateIdForUserPage, validateUserQuery } from "../middlewares/userSchemaValidation.middleware.js";
 
 const userRouter = Router();
 
 
 userRouter.post('/user/query', validateUserQuery, searchUserQuery);
-userRouter.get('/user/:id', validateIdForUserPage, getUserInUserPage);
+userRouter.get('/user/:id', authValidation, validateIdForUserPage, getUserInUserPage);
 
 
 
