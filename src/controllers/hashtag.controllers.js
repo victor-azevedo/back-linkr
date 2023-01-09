ae668bbbf8d4416ffb6ceb3f7aeba719e9fb829a
Victor Azevedo <josevcazevedo@gmail.com>
2023-01-09 19:34:17 -0300

    refactor: error messages

--- src/controllers/hashtag.controllers.js
+++ src/controllers/hashtag.controllers.js
@@ -1,49 +1,60 @@
-import { getPostsByHashtag, getUsersById, rankingHashtags } from "../repository/hashtag.repositories.js";
+import {
+  getPostsByHashtag,
+  getUsersById,
+  rankingHashtags,
+} from "../repository/hashtag.repositories.js";
 import urlMetadata from "url-metadata";
+import chalk from "chalk";
+import dayjs from "dayjs";
 
 async function getPostsByHashtags(req, res) {
-    const { hashtag } = req.params;
-    try {
-        const posts = await getPostsByHashtag(hashtag);
-        
-        if (posts.rowCount === 0) {
-            return res.status(200).send("There are no post yet");}
+  const { hashtag } = req.params;
+  try {
+    const posts = await getPostsByHashtag(hashtag);
 
-        const object = [];
-        for(let i = 0; i < posts.length; i++) {
-            const users = await getUsersById(posts[i].userId);
-            const linkMetadata = await urlMetadata(posts[i].linkUrl);
-            const { title, description, image } = linkMetadata;
-            let post = {
-                userid: users[0].id,
-                username: users[0].username,
-                userPictureUrl: users[0].pictureUrl,
-                link: posts[i].linkUrl,
-                text: posts[i].text,
-                linkMetadata: { title, description, image },
-                linkIsliked: posts[i].linkIsliked,}
-                
-            object.push(post);
-        }
-        return res.status(200).send(object);
-    }catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
+    if (posts.rowCount === 0) {
+      return res.status(200).send("There are no post yet");
     }
+
+    const object = [];
+    for (let i = 0; i < posts.length; i++) {
+      const users = await getUsersById(posts[i].userId);
+      const linkMetadata = await urlMetadata(posts[i].linkUrl);
+      const { title, description, image } = linkMetadata;
+      let post = {
+        userid: users[0].id,
+        username: users[0].username,
+        userPictureUrl: users[0].pictureUrl,
+        link: posts[i].linkUrl,
+        text: posts[i].text,
+        linkMetadata: { title, description, image },
+        linkIsliked: posts[i].linkIsliked,
+      };
+
+      object.push(post);
+    }
+    return res.status(200).send(object);
+  } catch (error) {
+    console.log(
+      chalk.redBright(dayjs().format("YYYY-MM-DD HH:mm:ss"), error.message)
+    );
+    return res.status(500).send(error);
+  }
 }
 
 async function getRankingHashtags(req, res) {
-    try {
-        const hashtags = await rankingHashtags();
-        return res.status(200).send(hashtags);
-    }catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
-}
+  try {
+    const hashtags = await rankingHashtags();
+    return res.status(200).send(hashtags);
+  } catch (error) {
+    console.log(
+      chalk.redBright(dayjs().format("YYYY-MM-DD HH:mm:ss"), error.message)
+    );
+    return res.status(500).send(error);
+  }
 }
 
 export { getPostsByHashtags, getRankingHashtags };
-
 
 // id,
 // username,
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,31 +1,41 @@
+import chalk from "chalk";
+import dayjs from "dayjs";
 import urlMetadata from "url-metadata";
-import connection, { hashLinkrsTb, hashtagsTb, linkrsTb, usersTb } from "../database/db.js";
+import connection, {
+  hashLinkrsTb,
+  hashtagsTb,
+  linkrsTb,
+  usersTb,
+} from "../database/db.js";
 import { usersLikedLinks } from "../repository/linkrs.repositories.js";
 
 export async function searchUserQuery(req, res) {
-    try {
-        const { searchText } = res.locals;
+  try {
+    const { searchText } = res.locals;
 
-        const { rows: users } = await connection.query(
-            ` 
+    const { rows: users } = await connection.query(
+      ` 
             SELECT id, username, "pictureUrl"
             FROM ${usersTb}
             WHERE username ILIKE CONCAT($1::text, '%') 
         `,
-            [searchText]
-        );
+      [searchText]
+    );
 
-        res.status(200).send(users);
-    } catch (err) {
-        res.send(err);
-        console.log(err);
-    }
+    res.status(200).send(users);
+  } catch (error) {
+    console.log(
+      chalk.redBright(dayjs().format("YYYY-MM-DD HH:mm:ss"), error.message)
+    );
+    return res.send(error);
+  }
 }
 
 export async function getUserInUserPage(req, res) {
-    try {
-        const userId = res.locals.user.id;
-        const { rows: linkrs } = await connection.query(`
+  try {
+    const userId = res.locals.user.id;
+    const { rows: linkrs } = await connection.query(
+      `
         SELECT l.id, l."linkUrl", l.text, l."userId", json_agg(h."hashtag") as "hashtags", u.username, u."pictureUrl"
         FROM ${linkrsTb} l
         LEFT JOIN ${hashLinkrsTb} hl ON l.id = hl."linkId"
@@ -34,68 +44,71 @@ export async function getUserInUserPage(req, res) {
         WHERE l."userId" = $1
         GROUP BY l.id, l."linkUrl", l.text, l."userId", u.id
         
-        `, [userId]);
-        
-        const links = linkrs;
+        `,
+      [userId]
+    );
 
-        const linksWithMetadata = await Promise.all(
-          links.map(async (link) => {
-            try {
-              const linkMetadata = await urlMetadata(link.linkUrl);
-              const { title, description, image } = linkMetadata;
-              const linkWithMetadata = {
-                ...link,
-                linkMetadata: { title, description, image },
-              };
-              return linkWithMetadata;
-            } catch (error) {
-              return {
-                ...link,
-                linkMetadata: {
-                  title: "",
-                  description: "",
-                  image:
-                    "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930",
-                },
-              };
+    const links = linkrs;
+
+    const linksWithMetadata = await Promise.all(
+      links.map(async (link) => {
+        try {
+          const linkMetadata = await urlMetadata(link.linkUrl);
+          const { title, description, image } = linkMetadata;
+          const linkWithMetadata = {
+            ...link,
+            linkMetadata: { title, description, image },
+          };
+          return linkWithMetadata;
+        } catch (error) {
+          return {
+            ...link,
+            linkMetadata: {
+              title: "",
+              description: "",
+              image:
+                "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930",
+            },
+          };
+        }
+      })
+    );
+
+    const queryLikesResult = await usersLikedLinks(userId);
+    const linksLikes = [...queryLikesResult.rows];
+
+    const linksWithMetadataAndLikes = linksWithMetadata.map(
+      (linkWithMetadata) => {
+        const linkLikesFound = linksLikes.find(
+          ({ linkId }) => Number(linkId) === Number(linkWithMetadata.id)
+        );
+        const linkIsLikedByUser = linkWithMetadata.likerId ? true : false;
+        delete linkWithMetadata.likerId;
+        return linkLikesFound
+          ? {
+              ...linkWithMetadata,
+              likes: {
+                linkIsLikedByUser,
+                usersLiked: [...linkLikesFound.likers],
+                count: linkLikesFound.likers.length,
+              },
             }
-          })
-        );
+          : {
+              ...linkWithMetadata,
+              likes: {
+                linkIsLikedByUser,
+                usersLiked: [],
+                count: 0,
+              },
+            };
+      }
+    );
 
-        const queryLikesResult = await usersLikedLinks(userId);
-        const linksLikes = [...queryLikesResult.rows];
-    
-        const linksWithMetadataAndLikes = linksWithMetadata.map(
-          (linkWithMetadata) => {
-            const linkLikesFound = linksLikes.find(
-              ({ linkId }) => Number(linkId) === Number(linkWithMetadata.id)
-            );
-            const linkIsLikedByUser = linkWithMetadata.likerId ? true : false;
-            delete linkWithMetadata.likerId;
-            return linkLikesFound
-              ? {
-                  ...linkWithMetadata,
-                  likes: {
-                    linkIsLikedByUser,
-                    usersLiked: [...linkLikesFound.likers],
-                    count: linkLikesFound.likers.length,
-                  },
-                }
-              : {
-                  ...linkWithMetadata,
-                  likes: {
-                    linkIsLikedByUser,
-                    usersLiked: [],
-                    count: 0,
-                  },
-                };
-          }
-        );
-    
-
-        res.status(200).send(linksWithMetadataAndLikes);
-    } catch (err) {
-        res.status(400).send(err);
-        console.log(err);
-    }
+    res.status(200).send(linksWithMetadataAndLikes);
+  } catch (error) {
+    console.log(
+      chalk.redBright(dayjs().format("YYYY-MM-DD HH:mm:ss"), error.message)
+    );
+    res.status(400).send(error);
+  }
 }
