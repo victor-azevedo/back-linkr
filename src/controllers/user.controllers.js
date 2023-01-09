a0b5d9bf532fe3db77bca348d61c982f6b7f22fa
Yuri Desideri <yuridesideri12@gmail.com>
2023-01-09 14:06:56 -0300

    fix: userPage response

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,6 @@
+import urlMetadata from "url-metadata";
 import connection, { hashLinkrsTb, hashtagsTb, linkrsTb, usersTb } from "../database/db.js";
+import { usersLikedLinks } from "../repository/linkrs.repositories.js";
 
 export async function searchUserQuery(req, res) {
     try {
@@ -22,7 +24,7 @@ export async function searchUserQuery(req, res) {
 
 export async function getUserInUserPage(req, res) {
     try {
-        const { userId } = res.locals;
+        const userId = res.locals.user.id;
         const { rows: linkrs } = await connection.query(`
         SELECT l.id, l."linkUrl", l.text, l."userId", json_agg(h."hashtag") as "hashtags", u.username, u."pictureUrl"
         FROM ${linkrsTb} l
@@ -33,8 +35,65 @@ export async function getUserInUserPage(req, res) {
         GROUP BY l.id, l."linkUrl", l.text, l."userId", u.id
         
         `, [userId]);
+        
+        const links = linkrs;
 
-        res.status(200).send(linkrs);
+        const linksWithMetadata = await Promise.all(
+          links.map(async (link) => {
+            try {
+              const linkMetadata = await urlMetadata(link.linkUrl);
+              const { title, description, image } = linkMetadata;
+              const linkWithMetadata = {
+                ...link,
+                linkMetadata: { title, description, image },
+              };
+              return linkWithMetadata;
+            } catch (error) {
+              return {
+                ...link,
+                linkMetadata: {
+                  title: "",
+                  description: "",
+                  image:
+                    "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930",
+                },
+              };
+            }
+          })
+        );
+
+        const queryLikesResult = await usersLikedLinks(userId);
+        const linksLikes = [...queryLikesResult.rows];
+    
+        const linksWithMetadataAndLikes = linksWithMetadata.map(
+          (linkWithMetadata) => {
+            const linkLikesFound = linksLikes.find(
+              ({ linkId }) => Number(linkId) === Number(linkWithMetadata.id)
+            );
+            const linkIsLikedByUser = linkWithMetadata.likerId ? true : false;
+            delete linkWithMetadata.likerId;
+            return linkLikesFound
+              ? {
+                  ...linkWithMetadata,
+                  likes: {
+                    linkIsLikedByUser,
+                    usersLiked: [...linkLikesFound.likers],
+                    count: linkLikesFound.likers.length,
+                  },
+                }
+              : {
+                  ...linkWithMetadata,
+                  likes: {
+                    linkIsLikedByUser,
+                    usersLiked: [],
+                    count: 0,
+                  },
+                };
+          }
+        );
+    
+
+        res.status(200).send(linksWithMetadataAndLikes);
     } catch (err) {
         res.status(400).send(err);
         console.log(err);
