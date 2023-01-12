cfa4cea227db8900a06615f28064353cb8cbff56
Victor Azevedo <josevcazevedo@gmail.com>
2023-01-12 12:34:27 -0300

    feat: add commenterId at response

--- src/repository/comments.repositories.js
+++ src/repository/comments.repositories.js
@@ -10,7 +10,7 @@ export function insertCommentDB(commenterId, commentText, linkId) {
 
 export function getCommentsDb(linkId) {
   return connection.query(
-    `SELECT c."commentText", u."username" AS "commenterName", u."pictureUrl" AS "commenterPicture"
+    `SELECT c."commentText", u."username" AS "commenterName", u."id" AS "commenterId", u."pictureUrl" AS "commenterPicture"
       FROM comments c
         JOIN users u ON c."commenterId" = u.id
         WHERE c."linkId" = $1`,
